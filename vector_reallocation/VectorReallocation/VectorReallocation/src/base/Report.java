0e04722c3af59624e0fc8f3a24286c6fbb5e0fce
movelikeriver <mars.lenjoy@gmail.com>
2014-08-18 21:40:56 -0700

    Support multiple thread in Java VectorReallocation

--- vector_reallocation/VectorReallocation/VectorReallocation/src/base/Report.java
+++ vector_reallocation/VectorReallocation/VectorReallocation/src/base/Report.java
@@ -1,5 +1,39 @@
 package base;
 
-public class Report {
+import java.util.Calendar;
+
+public class Report implements Runnable {
 	public int cost = -1;
+	private static final int RECUR_N = 27;  // don't try crazy num.
+	
+	private void update() {
+		this.cost = insaneCompute();
+	}
+	
+	private int insaneCompute() {
+		System.out.println("Start insaneCompute()...");
+	    long start = Calendar.getInstance().getTimeInMillis();
+		for (int i = 2; i < RECUR_N; i++) {
+			recur(i);
+		}
+		int costMs = (int)(Calendar.getInstance().getTimeInMillis() - start);
+		System.out.println("cost_ms: " + costMs);
+		return costMs;
+	}
+	
+	private int recur(int n) {
+		if (n <= 2) {
+			return n;
+		}
+		int sum = 0;
+		for (int i = 0; i < n; i++) {
+			sum += recur(i);
+		}
+		return sum;
+	}
+
+	@Override
+	public void run() {
+		update();	
+	}
 }
--- vector_reallocation/VectorReallocation/VectorReallocation/src/base/TaskManager.java
+++ vector_reallocation/VectorReallocation/VectorReallocation/src/base/TaskManager.java
@@ -1,6 +1,8 @@
 package base;
 
 import java.util.ArrayList;
+import java.util.List;
+
 import base.Report;
 
 public class TaskManager {
@@ -15,12 +17,33 @@ public class TaskManager {
 	}
 
 	public void run() {
+//		runSequentially();
+		runInParallel();
+	}
+
+	public void runSequentially() {
 		for (int i = 0; i < tasks.size(); i++) {
-			update(tasks.get(i));
+			tasks.get(i).run();
 		}
 	}
 
-	private void update(Report report) {
-		report.cost = 10;
+	public void runInParallel() {
+		List<Thread> threads = new ArrayList<Thread>();
+		for (int i = 0; i < tasks.size(); i++) {
+			Thread worker = new Thread(tasks.get(i));
+			worker.setName(String.valueOf(i));
+			worker.start();
+			threads.add(worker);
+		}
+
+		int running = 0;
+		do {
+			running = 0;
+			for (Thread thread : threads) {
+				if (thread.isAlive()) {
+					running++;
+				}
+			}
+		} while (running > 0);
 	}
 }
