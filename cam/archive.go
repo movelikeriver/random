db930ce33f2085836571795974df89a05f32f34a
movelikeriver <mars.lenjoy@gmail.com>
2014-09-21 16:37:54 -0400

    group snapshots by 6h instead of 1d; add index page

--- cam/archive.go
+++ cam/archive.go
@@ -2,21 +2,46 @@ package main
 
 import (
 	"flag"
-        "fmt"
-        "io"
+	"fmt"
+	"io"
 	"log"
-        "os"
-        "path"
-        "path/filepath"
+	"os"
+	"path"
+	"path/filepath"
+	"strconv"
 	"strings"
 )
 
 var dataDir = flag.String("data_dir", "", "")
 var destDir = flag.String("dest_dir", "", "")
 
+func getRangeByHour(hh string) (rangeName string) {
+	h, _ := strconv.Atoi(hh)
+	h = h / 6
+	switch h {
+	case 0:
+		return "00_06"
+	case 1:
+		return "06_12"
+	case 2:
+		return "12_18"
+	case 3:
+		return "18_24"
+	}
+	return "NA_NA"
+}
+
+// For Foscam, the file is like: Schedule_20140921-103158.jpg
 func getDestFullName(fn string) (destFullName string) {
-	dirName := path.Join(*destDir, strings.Split(fn, "-")[0], fn)
-        return dirName
+	pairs := strings.Split(fn, "_")
+	tp, ts := pairs[0], pairs[1]
+	ymd := ts[0:8]
+	hh := ts[9:11]
+
+	dirName := path.Join(*destDir,
+		fmt.Sprintf("%s_%s_%s", tp, ymd, getRangeByHour(hh)),
+		fn)
+	return dirName
 }
 
 // CopyFile copies a file from src to dst. If src and dst files exist, and are
@@ -25,46 +50,46 @@ func getDestFullName(fn string) (destFullName string) {
 func copyFile(src, dst string) (err error) {
 	sfi, err := os.Stat(src)
 	if err != nil {
-	        log.Println(err)
+		log.Println(err)
 		return err
-        }
-        if !sfi.Mode().IsRegular() {
+	}
+	if !sfi.Mode().IsRegular() {
 		log.Println("!IsRegular()")
-                // cannot copy non-regular files (e.g., directories,
-	        // symlinks, devices, etc.)
+		// cannot copy non-regular files (e.g., directories,
+		// symlinks, devices, etc.)
 		return fmt.Errorf("CopyFile: non-regular source file %s (%q)", sfi.Name(), sfi.Mode().String())
 	}
 
-        dfi, err := os.Stat(dst)
+	dfi, err := os.Stat(dst)
 	if err != nil {
 		if !os.IsNotExist(err) {
 			log.Println(dst, " exists.")
 			return nil
 		}
 	} else {
-    if !(dfi.Mode().IsRegular()) {
+		if !(dfi.Mode().IsRegular()) {
 			log.Println("!IsRegular()")
-		  return fmt.Errorf("CopyFile: non-regular destination file %s (%q)", dfi.Name(), dfi.Mode().String())
-    }
-    if os.SameFile(sfi, dfi) {
-      log.Println("same file")
-      return nil
-    }
-  }
-  if err = os.Link(src, dst); err == nil {
-          log.Println("link")
-          return nil
-  }
+			return fmt.Errorf("CopyFile: non-regular destination file %s (%q)", dfi.Name(), dfi.Mode().String())
+		}
+		if os.SameFile(sfi, dfi) {
+			log.Println("same file")
+			return nil
+		}
+	}
+	if err = os.Link(src, dst); err == nil {
+		log.Println("link")
+		return nil
+	}
 
-  dirPath := path.Dir(dst)
-  os.Mkdir(dirPath, 0755)
+	dirPath := path.Dir(dst)
+	os.Mkdir(dirPath, 0755)
 
-  if err = copyFileContents(src, dst); err != nil {
-          log.Println("copy failed....")
-          log.Println(err)
-          return err
-  }
-  return nil
+	if err = copyFileContents(src, dst); err != nil {
+		log.Println("copy failed....")
+		log.Println(err)
+		return err
+	}
+	return nil
 }
 
 // copyFileContents copies the contents of the file named src to the file named
@@ -72,85 +97,85 @@ func copyFile(src, dst string) (err error) {
 // destination file exists, all it's contents will be replaced by the contents
 // of the source file.
 func copyFileContents(src, dst string) (err error) {
-        in, err := os.Open(src)
-        if err != nil {
-                return
-        }
-        defer in.Close()
-        out, err := os.OpenFile(dst, os.O_RDWR, 0644)
-        if err != nil {
-                return
-        }
+	in, err := os.Open(src)
+	if err != nil {
+		return
+	}
+	defer in.Close()
+	out, err := os.OpenFile(dst, os.O_RDWR, 0644)
+	if err != nil {
+		return
+	}
 
-        defer func() {
-                cerr := out.Close()
-                if err == nil {
-                        err = cerr
-                }
-        }()
-        if _, err = io.Copy(out, in); err != nil {
-                return
-        }
-        err = out.Sync()
-        return err
+	defer func() {
+		cerr := out.Close()
+		if err == nil {
+			err = cerr
+		}
+	}()
+	if _, err = io.Copy(out, in); err != nil {
+		return
+	}
+	err = out.Sync()
+	return err
 }
 
 func moveFile(srcFn string, desFn string) (e error) {
-        _, err := os.Stat(desFn)
-        if err != nil {
-                if !os.IsNotExist(err) {
-                        return nil
-                }
-        }
+	_, err := os.Stat(desFn)
+	if err != nil {
+		if !os.IsNotExist(err) {
+			return nil
+		}
+	}
 
-        dirPath := path.Dir(desFn)
-        os.Mkdir(dirPath, 0755)
-        err = os.Rename(srcFn, desFn)
-        if err != nil {
-                log.Println(err)
-                return err
-        }
+	dirPath := path.Dir(desFn)
+	os.Mkdir(dirPath, 0755)
+	err = os.Rename(srcFn, desFn)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
 
-        os.Chmod(desFn, 0644)
-        return nil
+	os.Chmod(desFn, 0644)
+	return nil
 }
 
 // start with path higher up the tree, say $HOME
 func walkDir(dirPath string) {
-        err := filepath.Walk(dirPath, walkFn)
-        if err != nil {
-                log.Printf("Err: %s", err)
-        }
+	err := filepath.Walk(dirPath, walkFn)
+	if err != nil {
+		log.Printf("Err: %s", err)
+	}
 }
 
 func walkFn(srcFile string, fi os.FileInfo, err error) (e error) {
-        if fi.IsDir() {
-                // one level dir
-                return nil
-        }
+	if fi.IsDir() {
+		// one level dir
+		return nil
+	}
 
-        // if the first character is a ".", then skip it as it's a hidden file
-        if strings.HasPrefix(fi.Name(), ".") {
-                return nil
-        }
+	// if the first character is a ".", then skip it as it's a hidden file
+	if strings.HasPrefix(fi.Name(), ".") {
+		return nil
+	}
 
-        destFullName := getDestFullName(fi.Name())
-        log.Printf("Filename: %s -> %s", srcFile, destFullName)
+	destFullName := getDestFullName(fi.Name())
+	log.Printf("Filename: %s -> %s", srcFile, destFullName)
 
-        copyFile(srcFile, destFullName)
+	copyFile(srcFile, destFullName)
 
-        log.Println("chmod: ", destFullName)
-        if err = os.Chmod(destFullName, 0644); err != nil {
-                log.Println(err)
-                return err
-        }
+	log.Println("chmod: ", destFullName)
+	if err = os.Chmod(destFullName, 0644); err != nil {
+		log.Println(err)
+		return err
+	}
 
-        return nil
+	return nil
 }
 
 func main() {
-        flag.Parse()
+	flag.Parse()
 
-        walkDir(*dataDir)
-        return
+	walkDir(*dataDir)
+	return
 }
--- cam/generate_pages.go
+++ cam/generate_pages.go
@@ -1,86 +1,88 @@
 package main
 
 import (
-        "bytes"
-        "flag"
-        "fmt"
-        "io"
-        "io/ioutil"
-        "log"
-        "os"
-        "path/filepath"
-        "strings"
+	"bytes"
+	"flag"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"os"
+	"path"
+	"path/filepath"
+	"strings"
 )
 
 var dataDir = flag.String("data_dir", "", "")
 
 // start with path higher up the tree, say $HOME
 func walkDir(dirPath string) {
-        err := filepath.Walk(dirPath, walkFn)
-        if err != nil {
-                log.Printf("Err: %s", err)
-        }
+	err := filepath.Walk(dirPath, walkFn)
+	if err != nil {
+		log.Printf("Err: %s", err)
+	}
+	generateIndexPage(*dataDir)
 }
 
 func walkFn(srcFile string, fi os.FileInfo, err error) (e error) {
-        if !fi.IsDir() {
-                // one level dir
-                return nil
-        }
+	if !fi.IsDir() {
+		// one level dir
+		return nil
+	}
 
-        if srcFile == *dataDir {
-                return nil
-        }
+	if srcFile == *dataDir {
+		return nil
+	}
 
-        generatePage(srcFile)
-        return nil
+	generateSinglePage(srcFile)
+	return nil
 }
 
 func saveToFile(filename string, content string) (e error) {
-        f, err := os.Create(filename)
-        if err != nil {
-                fmt.Println(err)
-        }
-        n, err := io.WriteString(f, content)
-        if err != nil {
-                fmt.Println(n, err)
-                                return err
-        }
-        f.Close()
-        os.Chmod(filename, 0644)
-        if err = os.Chmod(filename, 0644); err != nil {
-                log.Println(err)
-                return err
-        }
-        log.Println(filename, "generated!")
-        return nil
+	f, err := os.Create(filename)
+	if err != nil {
+		fmt.Println(err)
+	}
+	n, err := io.WriteString(f, content)
+	if err != nil {
+		fmt.Println(n, err)
+		return err
+	}
+	f.Close()
+	os.Chmod(filename, 0644)
+	if err = os.Chmod(filename, 0644); err != nil {
+		log.Println(err)
+		return err
+	}
+	log.Println(filename, "generated!")
+	return nil
 }
 
 func genLinksInDir(dirPath string) (ret string) {
-        const htmlStr = `
+	const htmlStr = `
   <div>
     <a href="%s">
       <img src="%s" alt="%s"/>
     </a>
   </div>`
 
-        files, _ := ioutil.ReadDir(dirPath)
-        var buffer bytes.Buffer
-        for _, f := range files {
-                segs := strings.Split(dirPath, string(os.PathSeparator))
-                imgPath := segs[len(segs)-1] + "/" + f.Name() // for html
-                buffer.WriteString(fmt.Sprintf(htmlStr, imgPath, imgPath, f.Name()))
+	files, _ := ioutil.ReadDir(dirPath)
+	var buffer bytes.Buffer
+	for _, f := range files {
+		segs := strings.Split(dirPath, string(os.PathSeparator))
+		imgPath := segs[len(segs)-1] + "/" + f.Name() // for html
+		buffer.WriteString(fmt.Sprintf(htmlStr, imgPath, imgPath, f.Name()))
 
-        }
-        return buffer.String()
+	}
+	return buffer.String()
 }
 
-func generatePage(workDir string) {
-        pagePath := workDir + "_test.html"
-        log.Println("Generating", pagePath, "...")
-        subHtml := genLinksInDir(workDir)
+func generateSinglePage(workDir string) {
+	pagePath := workDir + "_test.html"
+	log.Println("Generating", pagePath, "...")
+	subHtml := genLinksInDir(workDir)
 
-        const htmlTmp = `
+	const htmlTmp = `
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
@@ -95,8 +97,9 @@ func generatePage(workDir string) {
 </head>
 
 <body>
-<p>Schedule_2014ddmm_test.html</p>
-<p>MDAlarm_2014ddmm_test.html</p>
+  <p>
+    <a href="index.html">index</a>
+  </p>
 <div class="result">
 %s
 </div>
@@ -104,12 +107,55 @@ func generatePage(workDir string) {
 </body>
 </html>`
 
-        saveToFile(pagePath, fmt.Sprintf(htmlTmp, subHtml))
+	saveToFile(pagePath, fmt.Sprintf(htmlTmp, subHtml))
+}
+
+func generateIndexPage(workDir string) {
+
+	const liHtml = `<li><a href="%s">%s</a></li>`
+
+	const indexPageHtml = `
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <meta name="viewport"content="target-densitydpi=device-dpi, width=device-width" />
+
+  <style type="text/css">
+  </style>
+  <title>View Snapshots</title>
+</head>
+
+<body>
+  <ul>
+  %s
+  </ul>
+</body>
+
+</html>`
+
+	files, _ := ioutil.ReadDir(workDir)
+	var buffer bytes.Buffer
+	for _, f := range files {
+		if f.IsDir() {
+			continue
+		}
+		if strings.HasSuffix(f.Name(), "~") {
+			continue
+		}
+		if f.Name() == "index.html" {
+			continue
+		}
+
+		buffer.WriteString(fmt.Sprintf(liHtml, f.Name(), f.Name()))
+	}
+	pageContent := fmt.Sprintf(indexPageHtml, buffer.String())
+	saveToFile(path.Join(workDir, "index.html"), pageContent)
 }
 
 func main() {
-        flag.Parse()
+	flag.Parse()
 
-        walkDir(*dataDir)
-        return
+	walkDir(*dataDir)
+	return
 }
